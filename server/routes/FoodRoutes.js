c5290d81dc586ff1f742c212790556fd230bfaee
maheshkumar09104 <mahesh09104@gmail.com>
2026-04-27 11:15:58 +0530

    update and add file

--- server/routes/FoodRoutes.js
+++ server/routes/FoodRoutes.js
@@ -1,21 +1,14 @@
 import express from "express";
-import multer from "multer";
 import { protect } from "../middleware/AuthMiddleware.js";
 import { getAllFoods, getFoodById, createFood, updateFood, deleteFood } from "../controllers/FoodController.js";
+import { upload } from "../config/cloudinary.js";
 
 const router = express.Router();
 
-// Multer: save uploaded images into /uploads folder
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads/"),
-  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
-});
-const upload = multer({ storage });
-
-router.get("/",    getAllFoods);            // public
-router.get("/:id", getFoodById);           // public
-router.post("/",   protect, upload.single("image"), createFood); // admin
-router.put("/:id", protect, upload.single("image"), updateFood); // ← add upload.single
-router.delete("/:id", protect, deleteFood);// admin
+router.get("/", getAllFoods);
+router.get("/:id", getFoodById);
+router.post("/", protect, upload.single("image"), createFood);
+router.put("/:id", protect, upload.single("image"), updateFood);
+router.delete("/:id", protect, deleteFood);
 
 export default router;
